37370fbe5c580755b008bc801c8b33908287fa4a
okcoder1 <codershub.code@gmail.com>
2026-04-18 12:15:37 +0530

    chore: lint

--- lib/check-if-changed.ts
+++ lib/check-if-changed.ts
@@ -5,16 +5,16 @@ import { getInput } from "./input";
 const execute = async (command: string): Promise<number> => {
   const options = {
     ignoreReturnCode: true,
-    silent: true
+    silent: true,
   };
   return await exec.exec(command, null, options);
 };
 
 const ifFilesChanged = async (): Promise<string> => {
   const files = getInput("files", { required: false }).trim().split("\n");
-  let changed: string[] = [];
+  const changed: string[] = [];
   for (const file of files) {
-    let exitCode = await execute(`git diff --exit-code ${file}`);
+    const exitCode = await execute(`git diff --exit-code ${file}`);
     if (exitCode !== 0) {
       changed.push(file);
     }
